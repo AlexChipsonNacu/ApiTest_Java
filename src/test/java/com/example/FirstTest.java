37244185265b9b5d1a547b6966c853ed833c55d0
Alex <manag410@gmail.com>
2025-02-11 12:05:57 +0200

    fix selectors, test passed

--- src/test/java/com/example/FirstTest.java
+++ src/test/java/com/example/FirstTest.java
@@ -10,18 +10,20 @@ public class FirstTest {
     void shouldFindSelenideRepositoryInGithub(){
         // Открывается страница Selenide и нажимается Enter
         open("https://github.com/");
+        // Нажимаем на поле поиск
+        $("[data-target=\"qbsearch-input.inputButton\"]").click();
         // Вводим в поле поиска selenide и нажимаем Enter
-        $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
         // Нажимаем на линк от первого результата поиска
-        $$("ul.repo-list li").first().$("a").click();
+        $$("[data-testid=\"results-list\"]").first().$("a").click();
         // Проверяем что в заголовке встречается selenide/selenide
-        $("h1").shouldHave(text("selenide/selenide"));
+        $("[data-turbo-frame=\"repo-content-turbo-frame\"]").shouldHave(text("selenide"));
         // Переходим в раздел Wiki проекта
         $("#wiki-tab").click();
         //Проверяем что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-body .markdown-body").shouldHave(text("Soft Assertions"));
         // Открываем страницу SoftAssertions,
-        $x("(//a[@href='/selenide/selenide/wiki/SoftAssertions'])[2]").click();
+        $x("(//a[@href='/selenide/selenide/wiki/SoftAssertions'])").click();
         // Проверяем, что внутри есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
     }
